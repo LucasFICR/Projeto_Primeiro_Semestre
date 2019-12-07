f7e5a88c257f9565c4e59a631eb0b5af399e3570
Lucas Alves <jose.alves@a.ficr.edu.br>
2019-12-07 13:00:23 -0300

    correção de bugs pagina home

--- NewPsy/js/auto.js
+++ NewPsy/js/auto.js
@@ -28,6 +28,46 @@ M.Slider.init(slider, {
   height: 600,
   transition: 500,
   interval: 10000
+}); //AutoComplete
+
+$(document).ready(function() {
+  $("input.autocomplete").autocomplete({
+    data: {
+      "Ana Clara - Psicologa": null,
+      "Carlos Santiago - Psicologo": null,
+      "Daniel Marinho - Psicologo": null,
+      "Nome_SobreN - Psicolog@": null
+    }
+  });
 });
 
-//
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+  ga("send", "event", "galeria", "next_prev", "Titulo da página");
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+/***/
