aa6a057bdb87835ca6ae14e216539b0ecefb4aea
Lucas Alves <jose.alves@a.ficr.edu.br>
2019-12-12 16:26:49 -0300

    correçao em bugs

--- NewPsy/js/validate.js
+++ NewPsy/js/validate.js
@@ -1,68 +1,69 @@
-const form = document.getElementById('form-login');
+const form = document.getElementById("form-login");
 
 const users = [
-    {
-        email: 'admin@admin',
-        senha: 'adminroot'
-    },
+  {
+    email: "admin@admin",
+    senha: "adminroot"
+  },
 
-    {
-        email: 'lucasficr123@gmail.com',
-        senha: '123456'
-    },
-]
+  {
+    email: "andre.martins@a.ficr.edu.br",
+    senha: "123456"
+  }
+];
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
 
-    //INPUTS do usuário
-    var inMail = document.getElementById('inputEmail').value;
-    var inSenha = document.getElementById('inputPassword').value;
+  //INPUTS do usuário
+  var inMail = document.getElementById("inputEmail").value;
+  var inSenha = document.getElementById("inputPassword").value;
 
-    //console.log(inMail, inSenha); - teste de valores no console
+  //console.log(inMail, inSenha); - teste de valores no console
 
-    function mailCheck() {
-        if (inMail != user.email) {
-            Swal.fire({
-                position: 'center',
-                icon: 'error',
-                title: 'Algo deu errado :(',
-                showConfirmButton: false,
-            })
-        } else {
-            passCheck();
-        }
+  function mailCheck() {
+    if (inMail != user.email) {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Algo deu errado :(",
+        showConfirmButton: false
+      });
+    } else {
+      passCheck();
     }
+  }
 
-    function passCheck() {
-        if (inSenha == user.senha) {
-            // exibir um alerta ok!
-            Swal.fire({
-                position: 'center',
-                icon: 'success',
-                title: 'Bem-vindo ao -Nome-',
-                showConfirmButton: false,
-            })
+  function passCheck() {
+    if (inSenha == user.senha) {
+      // exibir um alerta ok!
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title:
+          "Bem-vindo ao SOPE - Serviço de Orientação Psicológica Estudantil",
+        showConfirmButton: false
+      });
 
-            let btn = document.getElementById('btn-entrar');
+      let btn = document.getElementById("btn-entrar");
 
-            btn.setAttribute('disabled', true);
+      btn.setAttribute("disabled", true);
 
-            setTimeout(function () {
-                // mudar a tela ok!
-                window.location.href = '../home.html';
-            }, 2000);
-        } else {
-            Swal.fire({
-                position: 'center',
-                icon: 'error',
-                title: 'Algo deu errado :(',
-                showConfirmButton: false,
-            })
-        }
+      setTimeout(function() {
+        // mudar a tela ok!
+        window.location.href = "../home.html";
+      }, 2000);
+    } else {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Dados incorretos :(",
+        showConfirmButton: false
+      });
     }
+  }
 
-    for (user of users) {
-        mailCheck();
-    }
+  for (user of users) {
+    mailCheck();
+  }
 });
