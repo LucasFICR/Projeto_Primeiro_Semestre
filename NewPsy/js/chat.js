bdac9c1f7873b3adc1015ab27d5d269dc8b20600
Lucas Alves <jose.alves@a.ficr.edu.br>
2019-12-13 04:30:25 -0300

    correçoes de bugs

--- NewPsy/js/chat.js
+++ NewPsy/js/chat.js
@@ -3,3 +3,10 @@ $(".dropdown-trigger").dropdown();
 $(document).ready(function() {
   $(".modal").modal();
 });
+
+$(document).ready(function() {
+  $("select").formSelect();
+});
+
+$("#textarea1").val("");
+M.textareaAutoResize($("#textarea1"));
